6987f9d8d7c12e283b504124bd16aa9ae8cb685b
MorrisMedia <106269473+MorrisMedia@users.noreply.github.com>
2026-04-28 14:56:19 -0700

    feat: add homelife.truos.ai to server action allowedOrigins

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  experimental: { serverActions: { allowedOrigins: ['truos.ai', 'www.truos.ai', 'localhost:3000', 'truos.vercel.app'] } },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'truos.ai',
+        'www.truos.ai',
+        'homelife.truos.ai',
+        'localhost:3000',
+        'truos.vercel.app',
+      ],
+    },
+  },
   async rewrites() {
     return [
       { source: '/start', destination: '/start/index.html' },
